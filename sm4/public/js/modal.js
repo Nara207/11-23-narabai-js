2f0304cb4b91d9eae4215e6320392cf157a0508d
Nara Bai <baianara207@gmail.com>
2024-11-05 23:13:09 +0300

    sm4_1

--- sm4/public/js/modal.js
+++
@@ -1,9 +0,0 @@
-function OpenModal() {
-    const modal = document.querySelector('.modal');
-    modal.classList.add('open');
-     console.log(modal);
-  }
-  function closeModal() {
-    const modal = document.querySelector(".modal");
-    modal.classList.remove('open');
-}
